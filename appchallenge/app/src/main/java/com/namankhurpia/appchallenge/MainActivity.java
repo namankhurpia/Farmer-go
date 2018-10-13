55d8da52268ffdd75685d4fa23fd9ad77fa8fbd5
namankhurpia <naman.khurpia2016@vitstudent.ac.in>
2018-10-13 15:52:51 +0530

    home page added
    home page added

--- appchallenge/app/src/main/java/com/namankhurpia/appchallenge/MainActivity.java
+++ appchallenge/app/src/main/java/com/namankhurpia/appchallenge/MainActivity.java
@@ -1,5 +1,6 @@
 package com.namankhurpia.appchallenge;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.WindowManager;
@@ -17,7 +18,8 @@ public class MainActivity extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
-
+Intent i =new Intent(MainActivity.this,homescreen.class);
+startActivity(i);
 
 
 
