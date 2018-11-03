4cf3f04a727eb3f3e32c2ed7e5b3bded08ae7b57
namankhurpia <naman.khurpia2016@vitstudent.ac.in>
2018-11-03 23:44:50 +0530

    payment side
    payment side

---
+++ appchallenge/app/src/main/java/com/namankhurpia/appchallenge/payment_bank.java
@@ -0,0 +1,13 @@
+package com.namankhurpia.appchallenge;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class payment_bank extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment_bank);
+    }
+}
